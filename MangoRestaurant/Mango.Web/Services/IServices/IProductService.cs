adf863d61fe45d0a12fef8a07b55f9ea3c05384c
gsdr1992 <gabriel_silveira_r@hotmail.com>
2021-09-28 22:53:43 -0300

    Stopped on class 33

---
+++ MangoRestaurant/Mango.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Services.IServices
+{
+    public interface IProductService : IBaseService
+    {
+        Task<T> GetAllProductsAsync<T>();
+        Task<T> GetAllProductByIDAsync<T>(int id);
+        Task<T> CreateProductAsync<T>(ProductDto dto);
+        Task<T> UpdateProductAsync<T>(ProductDto dto);
+        Task<T> DeleteProductAsync<T>(int id);
+
+    }
+}
